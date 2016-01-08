a0819dcc20e901e74dc2c3835daf59f7286fe979
Sfinxul <sfinxul@gmail.com>
2016-01-08 19:15:47 +0200

    Update click.js

--- click.js
+++ click.js
@@ -1,3 +1,10 @@
-$('button').onclick(function(){
+$(button).onclick(function(){
 alert.log("You clicked!");
 });
+
+
+<script>
+function myFunction() {
+    document.getElementById("button").onclick = alert.log("You clicked!");
+}
+</script>
